82dab979f3e0db85c42187a258e7992ebf0367b5
JieSentry <195247833@qq.com>
2026-09-26 18:49:53 +0800

    Update mod.rs

--- backend/src/tracker/mod.rs
+++ backend/src/tracker/mod.rs
@@ -6,7 +6,7 @@ mod kalman_filter;
 mod strack;
 
 pub use bytetracker::{ByteTracker, IouGating};  
-pub use strack::STrack;
+pub use strack::{STrack, TrackState};
 
 #[derive(Clone, Debug)]
 pub struct Detection {
